c4d1ff71a416b00e6ba3b3cc69b18aacc8a908b0
RyanLim4 <rrl23@ic.ac.uk>
2024-08-05 22:48:45 +0800

    5.4 completed

--- adt_examples/fibonacci.py
+++ adt_examples/fibonacci.py
@@ -5,5 +5,15 @@ class Fib:
     """Fibonacci class."""
 
     def __init__(self):
-        """Construct Fib object."""
-        pass
+        """Construct Fib starting at 0 and 1."""
+        self.cur = 0
+        self.next = 1
+
+    def __iter__(self):
+        """Implement iterator protocol."""
+        return self
+
+    def __next__(self):
+        """Iterate step."""
+        self.cur, self.next = self.next, self.cur + self.next
+        return self.next
